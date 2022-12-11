54fc5968b8c78dc2cae631174957c72a2cfcdbc2
Tito Maresca <tjm275@cornell.edu>
2022-12-11 01:13:46 -0500

    Add video and alt text

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,11 @@ import { AboutMeComponent } from './about-me/about-me.component';
 import { ResearchComponent } from './research/research.component';
 import { ContactMeComponent } from './contact-me/contact-me.component';
 import { ModalComponent } from './modal/modal.component';
+import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
+import {VgCoreModule} from '@videogular/ngx-videogular/core';
+import {VgControlsModule} from '@videogular/ngx-videogular/controls';
+import {VgOverlayPlayModule} from '@videogular/ngx-videogular/overlay-play';
+import {VgBufferingModule} from '@videogular/ngx-videogular/buffering';
 
 @NgModule({
   declarations: [
@@ -20,6 +25,11 @@ import { ModalComponent } from './modal/modal.component';
     ModalComponent
   ],
   imports: [
+    VgCoreModule,
+    VgControlsModule,
+    VgOverlayPlayModule,
+    VgBufferingModule,
+    NgxExtendedPdfViewerModule,
     PdfViewerModule,
     BrowserModule,
     RouterModule.forRoot([
